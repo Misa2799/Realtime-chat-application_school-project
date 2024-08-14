73ff508bbf20836a3e9bdbbf75276c4599de0520
VictorSRV <155479482+VictorSRV@users.noreply.github.com>
2024-08-14 11:08:05 -0700

    imprement nav for status of user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 // link to register
 export const register = (req: Request, res: Response) => {
-  res.status(200).render("pages/auth/register", { error: null });
+  res.status(200).render("pages/auth/register", { error: null, title: "register" });
 };
 
 //link to login
@@ -10,7 +10,7 @@ export const login = (req: Request, res: Response) => {
   // if (req.session && req.session.user) {
   //   return res.status(400).render("pages/auth/login", { error: "Please logout" });
   // }
-  res.status(200).render("pages/auth/login", { error: null });
+  res.status(200).render("pages/auth/login", { error: null, title: "login" });
 };
 
 export const logout = (req: Request, res: Response) => {
