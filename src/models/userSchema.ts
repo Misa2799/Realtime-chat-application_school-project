816032e8f760885d9c6ca3db4d9576e211fa2219
VictorSRV <155479482+VictorSRV@users.noreply.github.com>
2024-08-14 09:43:22 -0700

    ignore commit

--- src/models/userSchema.ts
+++ src/models/userSchema.ts
@@ -1,1 +1,26 @@
-console.log("user model");
+import { model, Schema } from "mongoose"; 
+
+const userSchema = new Schema({
+  idNumber: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+const User = model("User", userSchema);
+
+export default User;
