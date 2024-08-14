93e9dac4a646ec22d8209d4dee58a2ddd17f2326
yusuken1121 <yusuken1121@gmail.com>
2024-08-14 11:14:38 -0700

    ✨ Added the connection to MongoDB code in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import mongoose from "mongoose";
 import { app } from "./app";
 import { ENV, PORT } from "./env";
 
-app.listen(PORT, () => {
-  console.log(`[server]: listening at http://localhost:${PORT} in ${ENV} mode`);
-});
+const dbUrl = process.env.DATABASE_PATH;
+if (!dbUrl) {
+  console.error("DB_URL is not defined");
+} else {
+  mongoose
+    .connect(dbUrl)
+    .then(() =>
+      app.listen(PORT, () => {
+        console.log(
+          `[server]: listening at http://localhost:${PORT} in ${ENV} mode`
+        );
+      })
+    )
+    .catch((error) => console.log(error));
+}
