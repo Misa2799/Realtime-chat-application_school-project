2e5972ba9590796c8d4be2309e3c006b55ef6135
yusuken1121 <yusuken1121@gmail.com>
2024-08-14 17:56:46 -0700

    ✨ Added the feature allowing users to choose a category and view chats from the database data.

--- src/controllers/chatsController.ts
+++ src/controllers/chatsController.ts
@@ -1,10 +1,27 @@
 import { Request, Response } from "express";
+import { Category } from "../models/categorySchema";
+import { Chat } from "../models/chatSchema";
 
 // Link to the chats page
-export const renderChatsPage = (req: Request, res: Response) => {
-  res.status(200).render("pages/chats/chat", { error: null, title: "chat" }); // we should pass the variables about categories and chats from DB or hard-coded
+export const renderChatsPage = async (req: Request, res: Response) => {
+  try {
+    const categories = await Category.find({});
+    console.log(categories);
+    const chats = await Chat.find({});
+    console.log(chats);
+    res.status(200).render("pages/chats/chat", {
+      error: null,
+      title: "chat",
+      categories,
+      chats,
+    });
+  } catch (error) {
+    console.error("cannot enter the room", error);
+  }
 };
 
 export const renderChatroomPage = (req: Request, res: Response) => {
-  res.status(200).render("pages/chats/chatroom", { error: null, title: "chatroom" }); // we should pass the variables about categories and chats from DB or hard-coded
+  res
+    .status(200)
+    .render("pages/chats/chatroom", { error: null, title: "chatroom" }); // we should pass the variables about categories and chats from DB or hard-coded
 };
